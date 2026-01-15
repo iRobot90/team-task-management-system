04c2d8bada9d141baf698baf892854ab998ce097
iRobot90 <amiandajesse27@gmail.com>
2026-01-15 14:42:11 +0300

    Fix potential collectstatic error by defining STATICFILES_DIRS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 STATIC_URL = 'django_static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = []
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
